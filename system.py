88907ae7e2dbcc7643b6071739e7dfbceb03080a
lakshya324 <lakshya.off31@gmail.com>
2023-12-04 01:21:49 +0530

    Version 2.9

--- system.py
+++ system.py
@@ -119,4 +119,4 @@ def result():
     #     return redirect(url_for("predict"))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000,debug=True)
